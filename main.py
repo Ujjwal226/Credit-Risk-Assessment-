2c801201213bd8f8d6d3668a7a4c504165d140ed
Ujjwal Khanna <ukhanna891@gmail.com>
2026-09-20 14:57:43 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+ml_model = {}
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ml_model["model"] = joblib.load("credit_risk_model.pkl")
+    ml_model["threshold"] = joblib.load("best_threshold.pkl")
+
+    print("Model loaded successfully")
+    print(f"Threshold: {float(ml_model['threshold'])}")
+
+    yield
+
+    ml_model.clear()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+class LoanApplication(BaseModel):
+    person_age: int
+    person_income: float
+    person_home_ownership: str
+    person_emp_length: float
+    loan_intent: str
+    loan_grade: str
+    loan_amnt: float
+    loan_int_rate: float
+    loan_percent_income: float
+    cb_person_default_on_file: str
+    cb_person_cred_hist_length: int
+
+
+@app.post("/predict")
+def predict(data: LoanApplication):
+
+    # Convert request data into DataFrame
+    input_df = pd.DataFrame([data.model_dump()])
+
+    # Get probability of default
+    probability = ml_model["model"].predict_proba(input_df)[0][1]
+
+    # Convert NumPy value to Python float
+    probability = float(probability)
+
+    # Convert threshold to Python float
+    threshold = float(ml_model["threshold"])
+
+    # Make binary prediction
+    prediction = int(probability >= threshold)
+
+    return {
+        "default_probability": probability,
+        "default_prediction": prediction,
+        "threshold": threshold,
+        "Result": "High Risk" if prediction == 1 else "Low Risk"
+    }
+
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
